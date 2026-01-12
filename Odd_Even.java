eb8666307de6974555fd524a5431ca74df9461fc
Milind1809 <msomaiya36@gmail.com>
2026-01-12 11:48:17 +0530

    Add files via upload

---
+++ Odd_Even.java
@@ -0,0 +1,11 @@
+public class Odd_Even {
+    public static void main(String[] args) {
+        int num = 9;
+
+        if (num%2==0){
+            System.out.println(num + " is an Even number.");
+        } else {
+            System.out.println(num + " is an Odd number.");
+        }
+}
+}
